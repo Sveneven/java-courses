0f736c1265c75a9b2927834c1811ea27bc00ac25
Роман Кисель < >
2016-09-12 17:30:09 +0300

    create Calculate

--- Calculate.java
+++ Calculate.java
@@ -3,5 +3,16 @@ public class Calculate{
 	public static void main(String[] arg){
 
 	System.out.println("Hello World");
+	int first = Integer.valueOf(arg[0]);
+	int second = Integer.valueOf(arg[1]);
+	int sum = first + second;
+	int div = first/second;
+	int rem = first%second;
+	int dif = first - second;
+	System.out.println("Sum " + sum);
+	System.out.println("dif " + dif);
+	System.out.println("div " + div);
+	System.out.println("rem " + rem);
+	
 }
 }
